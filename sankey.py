c8bf36a2d344db7a422872997f7393cf2eb817d0
Eddie Atkinson <45678264+eddie-atkinson@users.noreply.github.com>
2023-12-17 11:08:46 +0800

    Add 2023 votes and new index file

--- sankey.py
+++ sankey.py
@@ -1,31 +1,41 @@
 import plotly.graph_objects as go
+from datetime import datetime
 
 label = [
-  "Josh", 
-  "James", 
-  "Fraser",
-  "Stephen",
-  "Gubz", 
-  "Jesse",
-  "Eddie",
-  "Torin",
-  "Marko",
-  "1st preference",
+    "Josh",
+    "James",
+    "Fraser",
+    "Stephen",
+    "Gubz",
+    "Jesse",
+    "Eddie",
+    "Sam",
+    "Torin",
+    "Marko",
+    "1st preference",
 ]
 
-fig = go.Figure(data=[go.Sankey(
-    node = dict(
-      pad = 15,
-      thickness = 20,
-      line = dict(color = "black", width = 0.5),
-      label=label,
-      color = "blue"
-    ),
-    link = dict(
-      source = [9, 9, 9, 9, 9, 1, 3,6,],
-      target = [1,2, 8, 6, 3, 2, 2, 2],
-      value = [1,3, 4, 3, 1, 1, 1, 3]
-  ))])
+fig = go.Figure(
+    data=[
+        go.Sankey(
+            node=dict(
+                pad=15,
+                thickness=20,
+                line=dict(color="black", width=0.5),
+                label=label,
+                color="blue",
+            ),
+            link=dict(
+                source=[10] * 5 + [7, 8, 8, 1],
+                target=[1, 3, 7, 8, 9, 9, 9, 3, 3],
+                value=[2, 6, 1, 2, 2, 1, 1, 1, 2],
+            ),
+        )
+    ]
+)
 
-fig.update_layout(title_text="Preference Flows for Secret Santa", font_size=10)
+fig.update_layout(
+    title_text=f"Preference Flows for Secret Santa {datetime.utcnow().year}",
+    font_size=10,
+)
 fig.show()
